3f06eb62b9cec3273e1e76fece5f0aefc5482ebf
gasaba02 <55165802+gasaba02@users.noreply.github.com>
2021-09-14 11:22:38 -0400

    Add files via upload

---
+++ Assignment1/WeakPasswordException.java
@@ -0,0 +1,15 @@
+package application;
+
+public class WeakPasswordException extends Exception {
+	
+	public WeakPasswordException() {
+		
+		super("The password is OK but weak - it contains fewer than 10 characters.");
+	}
+	
+	public WeakPasswordException(String message) {
+		super(message);
+		
+	}
+
+}
