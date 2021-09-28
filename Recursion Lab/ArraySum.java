4f5e016e45e26a9958cc052fc1ffa92c7de1750e
gasaba02 <55165802+gasaba02@users.noreply.github.com>
2021-09-28 11:03:21 -0400

    Add files via upload

---
+++ Recursion Lab/ArraySum.java
@@ -0,0 +1,13 @@
+
+public class ArraySum {
+
+	public int sumOfArray (Integer[] a,int index) {
+		if(index == 0) {
+			return a[0];
+		}else 
+			
+		return sumOfArray(a, index - 1) + a[index] ;
+
+	}
+
+}
